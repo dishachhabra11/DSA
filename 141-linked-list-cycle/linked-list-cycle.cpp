d61c4a0f9e25cc5b4d7ed3835a69f2d26ac9d26e
dishaChhabra <dishachhabra173@gmail.com>
2024-01-08 11:17:50 +0530

    Time: 8 ms (55.99%) | Memory: 8.5 MB (35.31%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,17 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head==NULL)return false;
-        if(head->next==NULL)return false;
+        if (head==NULL ||head->next==NULL)return false;
         
         ListNode* fast,*slow;
         fast=head->next;
         slow=head;
         int n=10000;
         while(n--){
+            //first condition is if fast pointer meets slow pointer then its a cycle
             if(fast==slow ){
                 return true;
             }
+            //second condition is if fast reaches a null node then there can be no cycle
            if( fast==NULL || fast->next==NULL ){
                return false;
            } 
