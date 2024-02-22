fb41b208823b2564524326ab4fab5bc179619e6a
dishaChhabra <dishachhabra173@gmail.com>
2024-02-22 13:31:50 +0530

    Time: 3 ms (98.27%) | Memory: 21.6 MB (48.70%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-
+//writing the height function
  int height(TreeNode* root){
      if(root == nullptr){
          return 0;
@@ -22,12 +22,19 @@ public:
      int hight=max(left,right)+1;
      return hight;
  }
+ //check the base case that if root is null then thetree is balanced 
+ //also to check is a tree with a rootnode called root is balanced we need to ckeck
+ //1 if its left subtree is balanced
+// 2)is its right subtree is balanced
+  //3)if the difference in the height of leftsubtree and right sub tree is less thanor equal to one  
     bool isBalanced(TreeNode* root) {
       if(root ==nullptr){
           return true;
       }
        bool left=isBalanced(root->left);
+       if(left ==false )return false;
        bool right=isBalanced(root->right);
+       if(right ==false )return false;
        bool balance=abs(height(root->left)-height(root->right)) <= 1;
        if(left && right && balance){
            return true;
