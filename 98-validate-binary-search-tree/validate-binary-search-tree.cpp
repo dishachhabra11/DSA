70b74a21cc7c62887b1b03c7cfa16e1560ba4534
dishaChhabra <dishachhabra173@gmail.com>
2024-03-07 13:56:15 +0530

    Time: 9 ms (47.24%) | Memory: 20.2 MB (66.73%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,11 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ auto init =[](){
+     ios::sync_with_stdio(0);
+     cin.tie(0);
+     cout.tie(0);
+     return 'c';
+ }();
+
 class Solution {
 public:
 
    bool validateBST(TreeNode* root,long min,long max){
        if(root ==NULL) return true;
+         if(root->val < min && root->val >max)return false;
        
        if(root->val > min && root->val <max && validateBST(root->left,min,root->val) &&validateBST(root->right,root->val,max))return true;
        return false;
