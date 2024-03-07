c6ae2ca6f3db908f438fd95869b265792d2e33ae
dishaChhabra <dishachhabra173@gmail.com>
2024-03-07 10:11:26 +0530

    Time: 72 ms (99.00%) | Memory: 84.6 MB (82.99%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -9,34 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-    int count = 1 ;
-public:
-    int goodNodes(TreeNode* root) {
-        isGood(root->left,root->val);
-        isGood(root->right, root->val);
-        root->left = NULL;
-        root->right = NULL;
-        return count;
-    }
-private:
-    void isGood(TreeNode* root, int g){
-        if(!root){
-            return;
-        }
-        if(root->val>=g){
-            g=root->val;
-            count++;
-        }
-        isGood(root->left, g);
-        isGood(root->right,g);
-
-    }
-};
-auto init = []()
+    auto init = []()
 { 
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     return 'c';
 }();
+class Solution {
+public:
+int n=1;
+    int goodNodes(TreeNode* root) {
+        
+        if(root==nullptr){
+            return 0;
+        }
+       TreeNode* left=root->left;
+       TreeNode* right=root->right;
+
+       if(left!=nullptr){
+           if(root->val <= left->val)n++;
+           else{
+               left->val=root->val;
+           }
+           };
+           
+       if(right != nullptr){if(root->val <= right->val)n++;
+       else{
+           right->val=root->val;
+       }};
+       
+       goodNodes(root->left);
+       goodNodes(root->right);
+
+   return n;
+        
+    }
+ 
+};
