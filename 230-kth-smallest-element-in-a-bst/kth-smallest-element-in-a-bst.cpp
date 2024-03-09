149a560a49747e5627e6212bc0ff9f3f71a5e3e7
dishaChhabra <dishachhabra173@gmail.com>
2024-03-09 13:07:47 +0530

    Time: 4 ms (97.71%) | Memory: 22.5 MB (89.91%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -33,6 +33,13 @@
     //  return -1;
         
     // }
+
+    auto init =[](){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        return 'c';
+    }();
 class Solution {
 public:
 int n=0;
