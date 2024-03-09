87fe0581167b52a03d57ddf3c0f43893bb38be04
dishaChhabra <dishachhabra173@gmail.com>
2024-03-09 13:05:45 +0530

    Time: 8 ms (88.96%) | Memory: 22.6 MB (69.61%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -37,37 +37,22 @@ class Solution {
 public:
 int n=0;
 
- int moveleft(TreeNode* root,int k){
+ void moveleft(TreeNode* root,int &k,int &ans){
      if(root==NULL){
-         return -2 ;
+         return  ;
      }
-    if(root->left !=nullptr) moveleft(root->left,k);
-    if(root!=nullptr)++n;
-    return n;
-    if(root->right !=nullptr)moveleft(root->right,k);
+    if(root->left !=nullptr) moveleft(root->left,k,ans);
+   k--;
+   if(k==0)ans=root->val;
+    if(root->right !=nullptr)moveleft(root->right,k,ans);
+
+   
 
  }
-  int kthSmallest(TreeNode* root, int k) {
-
-        priority_queue <int, vector<int>, greater<int>> gq;
-
-        queue <TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-           TreeNode* temp=q.front();
-           gq.push(temp->val);
-           q.pop();
-          if(temp->left !=NULL) q.push(temp->left);
-          if(temp->right !=NULL)q.push(temp->right);
- 
-        }
-        while(k--){
-            if(k==0){
-            return gq.top();
-            }
-            gq.pop();
-        }
-     return -1;
+int kthSmallest(TreeNode* root, int k) {
+int ans=-1;
+  moveleft(root,k,ans);
+  return ans;
         
-    }
+    };
 };
